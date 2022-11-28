4bbda245d3637bbffd92dc31ecd6a5ea0a206c3d
HENRY VERISSIMO <verissimohenry04@gmail.com>
2022-11-28 16:17:21 +0100

    Modify app.js file

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const selectedImage = document.getElementById('seclectedImage');
 const imageIndexes = [1,2,3,4,5,6,7,8,9,10];
 const selectedIndex = null;
 
-imageIndexes.forEach(i => {
+imageIndexes.forEach((i) => {
     const image = document.createElement('img');
-    image.src = '/images/cover__episode-$(i).jpg';
-    image.alt = 'Cover for Episode $(i) of the Compressed.fm Podcast';
+    image.src = '/images/cover__episode-${i}.jpg';
+    image.alt = 'Cover for Episode ${i} of the Compressed.fm Podcast';
     image.classList.add('galleryImg'); 
 
     image.addEventListener('click', () =>{
