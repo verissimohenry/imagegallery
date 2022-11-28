0fa519e1d125a778a6529f64e3f5ea9cf1127c37
HENRY VERISSIMO <verissimohenry04@gmail.com>
2022-11-28 15:47:46 +0100

    Modify app.js file

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('seclectedImage');
 const imageIndexes = [1,2,3,4,5,6,7,8,9,10];
 const selectedIndex = null;
+
+imageIndexes.forEach(i => {
+    const image = document.createElement('img');
+    image.src = '/images/cover__episode-$(i).jpg';
+    image.alt = 'Cover for Episode $(i) of the Compressed.fm Podcast';
+    image.classList.add('galleryImg'); 
+
+    image.addEventListener('click', () =>{
+        //popup staff
+    })
+
+    gallery.appendChild(image);
+});
