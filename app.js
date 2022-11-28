983f8e8e94569b88b811d0be982c4404fb1656c3
HENRY VERISSIMO <verissimohenry04@gmail.com>
2022-11-28 15:36:23 +0100

    Modify app.js file

---
+++ app.js
@@ -0,0 +1,5 @@
+const gallery = document.getElementById('gallery');
+const popup = document.getElementById('popup');
+const selectedImage = document.getElementById('seclectedImage');
+const imageIndexes = [1,2,3,4,5,6,7,8,9,10];
+const selectedIndex = null;
